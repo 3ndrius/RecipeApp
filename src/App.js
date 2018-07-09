d4a4f3784250d774a27939abc4871645de21d71c
3ndrius <andrzejo1991@gmail.com>
2018-07-09 16:43:16 +0200

    Mapping array

--- src/App.js
+++ src/App.js
@@ -12,19 +12,34 @@ const API_KEY = "53c2c4dd92ce54fe6bf9b7acbf8248c5";
 
 class App extends Component {
 
+  state = {
+    recipes: []
+  }
+
   getRecipe = async (e) => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
     const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
 
     const data = await api_call.json();
-    console.log(data);
+    this.setState({
+      recipes: data.recipes
+    });
+    console.log(this.state.recipes);
+    
     
   }
   render() {
     return (
       <div className="app">
         <Form getRecipe={this.getRecipe}/>
+        
+        {
+          this.state.recipes.map((recipe) => {
+            return <p key={recipe.recipe_id}> {recipe.title} </p>
+
+        }) 
+        }
        
       </div>
     );
