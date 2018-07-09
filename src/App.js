426815a10c243515695d329122a034f28f17832c
3ndrius <andrzejo1991@gmail.com>
2018-07-09 16:27:23 +0200

    Async call

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import './App.css';
 // own components
 import Form from "./components/Form";
 
+
+
+const API_KEY = "53c2c4dd92ce54fe6bf9b7acbf8248c5";
+
+
+
 class App extends Component {
 
-  getRecipe = (e) => {
-
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
-    console.log("Working");
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
+
+    const data = await api_call.json();
+    console.log(data);
+    
   }
-
-
   render() {
     return (
       <div className="app">
--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Form = props => (
     <form onSubmit={props.getRecipe}>
 
-        <input type="text" name="input" />
+        <input type="text" name="recipeName" />
         <button> Search </button>
 
     </form>
