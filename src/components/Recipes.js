0e76545338101eaed4197bc1309daee6e1bc7fec
3ndrius <andrzejo1991@gmail.com>
2018-07-10 10:25:27 +0200

    Adding router

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -6,7 +6,8 @@ const Recipes = (props) => (
             return(
               <div key={recipe.recipe_id} className="grid-item"> 
                 <img src={recipe.image_url} alt={recipe.title} />
-                <h2 > {recipe.title} </h2>
+                <h4 className="title"> {recipe.title} </h4>
+                <p className="author">Publisher: <span>{recipe.publisher}</span> </p>
                 <button className="btn">More </button>
               </div>
             );
