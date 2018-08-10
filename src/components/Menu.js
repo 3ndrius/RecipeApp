370b694776b574d6194aa9955431f4654dcfa244
3ndrius <andrzejo1991@gmail.com>
2018-08-10 21:19:26 +0200

    Adding menu, styling menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,15 +3,16 @@ import React, { Component } from 'react'
 
 const Menu = () => {
     return (
-      <div className="navbar">
+    <div className="wrap-nav" > 
+  <div className="navbar">
           <div className="logo"> <img src="/logo.jpg" alt="logo"/>  <h2>Gratia</h2></div>
           <ul className="nav">
             <li className="item">About</li>
             <li className="item">Menu</li>
-            <li className="item">Gallery</li>
             <li className="item">Contact</li>
           </ul>
       </div>
+    </div>
     )
 }
 export default Menu;
