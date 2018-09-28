ea2ae66951a4ce054ceec8c20b046e0dd19cd94a
3ndrius <3ndrius@users.noreply.github.com>
2018-09-28 23:14:09 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,12 @@
 import React from 'react'
-
  const Footer = () => {
     return (
       <div className="footer">
-
         <div className="footer-wrap">
             <div className="round"><img src="/Logo.png" alt="footer-logo"/></div>
             <h3 > Grantia Restaurant </h3>
-            
         </div>
         <p> All Rights Reserved | 2018 Grantia</p>
-        
       </div>
     )
 }
