d359d528c7b9349be8610f0f1782f23c4a619030
3ndrius <3ndrius@users.noreply.github.com>
2018-10-02 22:39:22 +0200

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,24 +1,17 @@
 import React from 'react'
 import { Link} from 'react-router-dom';
-
-
-
-
 const Menu = () => {
-    return (
-     
+    return ( 
     <div className="wrap-nav" > 
-  <div className="navbar">
-          <Link to="/"> <div className="logo"> <img src="/logo.jpg" alt="logo"/>  <h2>Gratia</h2></div> </Link>
-          <ul className="nav">
-             <Link to="/about"><li className="item">About</li></Link> 
+        <div className="navbar">
+           <Link to="/"> <div className="logo"> <img src="/logo.jpg" alt="logo"/>  <h2>Gratia</h2></div> </Link>
+            <ul className="nav">
+            <Link to="/about"><li className="item">About</li></Link> 
             <Link to="/daily-menu"><li className="item">DailyMenu</li></Link>
-            <Link to="/contact"> <li className="item">Contact</li></Link>
-           
+            <Link to="/contact"> <li className="item">Contact</li></Link>        
           </ul>
       </div>
-    </div>
-   
+    </div> 
     )
 }
 export default Menu;
