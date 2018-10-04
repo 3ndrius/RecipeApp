68bfae69228d50f3c195d9871709455eba4184c3
3ndrius <3ndrius@users.noreply.github.com>
2018-10-04 23:30:05 +0200

    Update Recipes.js

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -4,18 +4,17 @@ import React, { Component } from 'react'
 export default class Recipes extends Component {
   render() {
    const {recipes} = this.props;
-
    const recipeList = recipes.map( recipe => {
     return (
       <div key={recipe.recipe_id} className="grid-item"> 
            <div className="figure-wrap">  <img src={recipe.image_url} alt={recipe.title} /> </div>
-             <h4 className="title"> {recipe.title.length > 25 ? recipe.title.slice(0, 25) : recipe.title }  </h4>
-             <p className="author">Publisher: <span>{recipe.publisher}</span> </p>
-              <Link to={{
-                pathname: `/recipe/${recipe.recipe_id}`,
-                state: {recipe: recipe.title}
-                }}><button className="btn"> View Recipe  </button></Link>                
-         </div>
+            <h4 className="title"> {recipe.title.length > 25 ? recipe.title.slice(0, 25) : recipe.title }  </h4>
+            <p className="author">Publisher: <span>{recipe.publisher}</span> </p>
+            <Link to={{
+            pathname: `/recipe/${recipe.recipe_id}`,
+            state: {recipe: recipe.title}
+           }}><button className="btn"> View Recipe  </button></Link>                
+       </div>
     )
    })
     return (
@@ -23,6 +22,5 @@ export default class Recipes extends Component {
       {recipeList} 
       </div>
     )
-  
   }
 }
